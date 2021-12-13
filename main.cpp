59da01fc112a2c9fd1bd88dec30407e1d129f60f
Vladislav Golovin <golovin.va@phystech.edu>
2021-12-13 14:07:19 +0300

    Passing primitives filename via arguments

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@ int main(int argc, char* argv[])
     if (argc > 1)
     {
         Mission mission;
-        if(argc == 5)
-            mission.setFileNames(argv[1], argv[2], argv[3], argv[4], nullptr);
-        else if(argc == 4)
-            mission.setFileNames(argv[1], argv[2], argv[3], nullptr,  nullptr);
-        else if(argc == 2)
-            mission.setFileNames(argv[1], argv[1], argv[1], argv[1], nullptr);
+        if(argc == 6)
+            mission.setFileNames(argv[1], argv[2], argv[3], argv[4], argv[5]);
+        else if(argc == 5)
+            mission.setFileNames(argv[1], argv[2], argv[3], nullptr, argv[4]);
+        else if(argc == 3)
+            mission.setFileNames(argv[1], argv[1], argv[1], argv[1], argv[2]);
         else
-        {
+        {   //TODO: Fix warning text
             std::cout<<"Wrong number of input XML-files. It should be either all-in-one file, or three ones: map-file, task-file and config-file. Additional file with obstacles is also allowed.\n";
             return 0;
         }
